11bc584b021557e0354831446ba7914e09136422
fsrocha-dev <contato.frankrocha@gmail.com>
2020-04-24 21:21:35 -0300

    create sequelize connection

--- src/db/strategies/postgres.js
+++ src/db/strategies/postgres.js
@@ -1,9 +1,26 @@
 const interfaceCrud = require('./interfaces/interfaceCrud')
+const Sequelize = require('sequelize')
+
 class Postgres extends interfaceCrud {
   constructor() {
     super()
+    this._driver = null
+    this._heroes = null
   }
+  isConnected() {
 
+  }
+  _connect() {
+    this._driver = new Sequelize(
+      'heroes',
+      'root',
+      'root', {
+      host: 'localhost',
+      dialect: 'postgres',
+      quoteIdentifiers: false,
+      operatorsAliases: false
+    })
+  }
   create(item) {
     console.log("item salvo em postgres")
   }

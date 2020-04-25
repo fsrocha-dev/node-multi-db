14c8506fae2b08c3ea2ef9278b7c161f5333d590
fsrocha-dev <contato.frankrocha@gmail.com>
2020-04-24 22:07:40 -0300

    create test postgres strategy

--- src/tests/postgressStrategy.js
+++
@@ -1,5 +0,0 @@
-const assert = require('assert')
-const Postgres = require('./../db/strategies/postgres')
-const Context = require('./../db/strategies/base/contextStrategy')
-
-const context = new Context(new Postgres())
